9ba1c59977b67a94f2432a17ae88d72d0790054e
Will <wruann8@gmail.com>
2023-05-18 12:30:40 -0400

    v3.1.2 OOP

--- datalogger/DataLogger.py
+++ datalogger/DataLogger.py
@@ -108,146 +108,148 @@ class DataLogger:
             if not os.path.exists(file_path):
                 message = f'{file_path} does not exist... creating file'
                 messagebox.showinfo(title='Warning', message=message)
-
-                if isinstance(self.df, pd.DataFrame) and not isinstance(self.df,list): # checks if input is a single DataFrame object 
-                    with pd.ExcelWriter(file_path, engine = 'openpyxl', mode = 'w') as writer: 
-                        self.df.to_excel(writer, sheet_name = 'Sheet1', index=False)
-                        sheet = writer.book['Sheet1']
-                        self.adjust_col_width(sheet)
-                        
-                elif isinstance(self.df, list) and all(isinstance(df, pd.DataFrame) for df in self.df): # checks if input is a list that ONLY contains dataframes 
-                    with pd.ExcelWriter(file_path, engine = 'openpyxl', mode = 'w') as writer: 
-                        for i, df in enumerate(self.df): # iterate through the elements and creating an index for naming
-                            sheet_name = df.name if hasattr(df, 'name') else f'Sheet{i+1}' # check for whether if dataframe(s) have attr 'name' and if it does wil be used 
-                            df.to_excel(writer, sheet_name = sheet_name, index = False) # either use existing name as sheet or use default with numerical suffix
-                            sheet = writer.book[sheet_name]
-                            self.adjust_col_width(sheet)
-
-                else: 
+                if isinstance(self.df, pd.DataFrame) and not isinstance(self.df, list):
+                    self.write_single_dataframe(file_path)
+                elif isinstance(self.df, list) and all(isinstance(df, pd.DataFrame) for df in self.df):
+                    self.write_multiple_dataframes(file_path)
+                else:
                     print('Var used is neither a dataframe nor a list of dataframes. Retry.')
                     raise ValueError
-                
             else:
                 message = f'{file_path} has been found... opening and appending'
                 messagebox.showinfo(title='File Found', message=message)
-
-                if isinstance(self.df, pd.DataFrame) and not isinstance(self.df,list): 
-                    with pd.ExcelWriter(file_path, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
-                        self.df.to_excel(writer, sheet_name='Sheet1', index=False)
-                        sheet = writer.book['Sheet1']
-                        self.adjust_col_width(sheet)
-                
-                elif isinstance(self.df, list) and all(isinstance(df, pd.DataFrame) for df in self.df): # checks if input is a list that ONLY contains dataframes 
-                    with pd.ExcelWriter(file_path, engine = 'openpyxl', mode = 'w') as writer: 
-                        for i, df in enumerate(self.df): # iterate through the elements and creating an index for naming
-                            sheet_name = df.name if hasattr(df, 'name') else f'Sheet{i+1}' # check for whether if dataframe(s) have attr 'name' and if it does wil be used 
-                            df.to_excel(writer, sheet_name = sheet_name, index = False) # either use existing name as sheet or use default with numerical suffix 
-                            sheet = writer.book[sheet_name]
-                            self.adjust_col_width(sheet)
-
-                else: 
+                if isinstance(self.df, pd.DataFrame) and not isinstance(self.df, list):
+                    self.append_single_dataframe(file_path)
+                elif isinstance(self.df, list) and all(isinstance(df, pd.DataFrame) for df in self.df):
+                    self.append_multiple_dataframes(file_path)
+                else:
                     print('Var used is neither a dataframe nor a list of dataframes. Retry.')
-                    raise ValueError 
-                
-            
+                    raise ValueError
             os.chdir(os.pardir)
-
-            self.open_file() 
+            self.open_file()
         except Exception as e:
             message = f'Error occurred:\n{e}\n\nRetrying with alternative method'
             messagebox.showinfo(title='Warning', message=message)
             sleep(3)
-
-            if isinstance(self.df, pd.DataFrame) and not isinstance(self.df, list): 
-                with pd.ExcelWriter(self.file_name, engine='openpyxl', mode='a', if_sheet_exists='replace', engine_kwargs = {'options' : {'sheet_state' : 'visible'}}) as writer:
-                    try: 
-                        if mimetypes.guess_type(self.file_name)[0] in ['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']:
-                            print('File is the formats: vnd.ms-excel or vnd.openxml formats')
-                            existing_df = pd.read_excel(self.file_name)
-                        else: 
-                            print('File is not the in the accepted pd.read_excel formats... using alternative method') 
-                            existing_file = pd.ExcelFile(self.file_name)
-                            existing_df = existing_file.parse(sheet_name = 'Sheet1')
-                    except Exception as e: 
-                        print(f'Error occured: {e}')
-        
-                    book = load_workbook(self.file_name)
-                    writer.book = book
-                    writer.sheets = {ws.title: ws for ws in writer.book.worksheets} # get all the sheets 
-
-                    existing_columns = set() # extract existing column names form the first sheet 
-                    for sheet in book.worksheets:
-                        if sheet.title == 'Sheet1':
-                            existing_df_sheet = pd.read_excel(self.file_name, sheet_name=sheet.title)
-                            existing_columns.update(existing_df_sheet.columns)
-
-
-                    new_columns = set(self.df.columns) # check if new data has the same column names as the existing data 
-                    if existing_columns == new_columns:
-                        new_df = pd.concat([existing_df_sheet, self.df], axis=0, ignore_index=True)
-                        last_row = len(existing_df_sheet)
-                        new_df.style.apply(lambda x: ['background-color: #C4D9F2' if x.name >= last_row else ''], axis=1) # change color for only newly appended data and default white if data is old 
-                        new_df.to_excel(writer, sheet_name='Sheet1', index=False)
-                        sheet = book['Sheet1']
-                        for row_idx, row in enumerate(sheet.iter_rows(min_row=2, min_col=1, values_only=True), start=2): # iterates over the rows of the worksheet starting from a set starting row. returns iterable of Cell objects for each row. Ensures we get the cell values instead of ALL of the cells' objects. Enumerate to get both the row index and the row values during iteration. Start =2 sets the starting value of the row index
-                            for col_idx, value in enumerate(row, 1):
-                                if row_idx > last_row + 1: 
-                                    cell = sheet.cell(row = row_idx, column = col_idx)
-                                    cell.fill = PatternFill(start_color='C4D9F2', end_color='C4D9F2', fill_type='solid')
-                                
-                        self.adjust_col_width(sheet)
-                    else:
-                        self.df.to_excel(writer, sheet_name='Sheet1', index=False)
-                        sheet = book['Sheet1']
-                        self.adjust_col_width(sheet)
-                                
-
+            if isinstance(self.df, pd.DataFrame) and not isinstance(self.df, list):
+                self.append_single_dataframe_alternative()
             elif isinstance(self.df, list) and all(isinstance(df, pd.DataFrame) for df in self.df):
-                with pd.ExcelWriter(self.file_name, engine='openpyxl', mode='a', if_sheet_exists='replace', engine_kwargs = {'options' : {'sheet_state' : 'visible'}}) as writer:
-                        try: 
-                            if mimetypes.guess_type(self.file_name)[0] in ['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']:
-                                print('File is the formats: vnd.ms-excel or vnd.openxml formats')
-                                existing_df = pd.read_excel(self.file_name)
-                            else: 
-                                print('File is not the in the accepted pd.read_excel formats... using alt method') 
-                                existing_file = pd.ExcelFile(self.file_name)
-                                existing_df = existing_file.parse(sheet_name = 'Sheet1')
-                        except Exception as e: 
-                            print(f'Error occured: {e}')
-
-                        new_data = []
-                        existing_columns = set() # Extract column names from the existing data into a set (different from a list because unique values only) 
-                        for sheet_name, df in existing_df.items():
-                            existing_columns.update(df.columns)
-
-                        for df in self.df:
-                            new_columns = set(df.columns) # Extract column names from the new dataframes into a set
-
-                            if existing_columns.intersection(new_columns): # If there are any overlapping columns, concatenate the new data with the existing data and add to the list of new data to be written
-                                existing_df_sheet = existing_df.get(df.name, pd.DataFrame()) # if there is a dataframe name then it takes that. if there isn't then default value 
-                                last_row = len(existing_df_sheet)
-                                new_df = pd.concat([existing_df_sheet, df], axis=0, ignore_index=True)
-                                new_df.style.apply(lambda x: ['background-color: #C4D9F2' if x.name >= last_row else ''], axis=1) # temporary function for creating background colors 
-                                new_data.append(new_df)
-                                for row_idx, row in enumerate(sheet.iter_rows(min_row=2, min_col=1, values_only=True), start=2): # iterates over the rows of the worksheet starting from a set starting row. returns iterable of Cell objects for each row. Ensures we get the cell values instead of ALL of the cells' objects. Enumerate to get both the row index and the row values during iteration. Start =2 sets the starting value of the row index
-                                    for col_idx, value in enumerate(row, 1):
-                                        if row_idx > last_row + 1: 
-                                            cell = sheet.cell(row = row_idx, column = col_idx)
-                                            cell.fill = PatternFill(start_color='C4D9F2', end_color='C4D9F2', fill_type='solid')
-                            else:
-                                new_data.append(df) # If there are no overlapping columns, it treats it as a new dataframe and add it to the list of dataframes 
-                            
-                        for df in new_data:
-                            df.to_excel(writer, sheet_name=df.name, index=False)
-                            sheet = writer.sheets[df.name]
-                            self.adjust_col_width(sheet)
-            else: 
+                self.append_multiple_dataframes_alternative()
+            else:
                 print('Var used is neither a dataframe nor a list of dataframes. Retry.')
-                raise ValueError 
-            
+                raise ValueError
             os.chdir(os.pardir)
+            self.open_file()
+    def write_single_dataframe(self, file_path):
+        with pd.ExcelWriter(file_path, engine='openpyxl', mode='w') as writer:
+            self.df.to_excel(writer, sheet_name='Sheet1', index=False)
+            sheet = writer.book['Sheet1']
+            self.adjust_col_width(sheet)
 
-            self.open_file() 
+    def write_multiple_dataframes(self, file_path):
+        with pd.ExcelWriter(file_path, engine='openpyxl', mode='w') as writer:
+            for i, df in enumerate(self.df):
+                sheet_name = df.name if hasattr(df, 'name') else f'Sheet{i+1}'
+                df.to_excel(writer, sheet_name=sheet_name, index=False)
+                sheet = writer.book[sheet_name]
+                self.adjust_col_width(sheet)
+
+    def append_single_dataframe(self, file_path):
+        with pd.ExcelWriter(file_path, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
+            self.df.to_excel(writer, sheet_name='Sheet1', index=False)
+            sheet = writer.book['Sheet1']
+            self.adjust_col_width(sheet)
+
+    def append_multiple_dataframes(self, file_path):
+        with pd.ExcelWriter(file_path, engine='openpyxl', mode='w') as writer:
+            for i, df in enumerate(self.df):
+                sheet_name = df.name if hasattr(df, 'name') else f'Sheet{i+1}'
+                df.to_excel(writer, sheet_name=sheet_name, index=False)
+                sheet = writer.book[sheet_name]
+                self.adjust_col_width(sheet)
+
+    def append_single_dataframe_alternative(self):
+        with pd.ExcelWriter(self.file_name, engine='openpyxl', mode='a', if_sheet_exists='replace', engine_kwargs={'options': {'sheet_state' : 'visible'}}) as writer:
+            try: 
+                if mimetypes.guess_type(self.file_name)[0] in ['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']:
+                    print('File is the formats: vnd.ms-excel or vnd.openxml formats')
+                    existing_df = pd.read_excel(self.file_name)
+                else: 
+                    print('File is not the in the accepted pd.read_excel formats... using alternative method') 
+                    existing_file = pd.ExcelFile(self.file_name)
+                    existing_df = existing_file.parse(sheet_name = 'Sheet1')
+            except Exception as e: 
+                print(f'Error occured: {e}')
+
+            book = load_workbook(self.file_name)
+            writer.book = book
+            writer.sheets = {ws.title: ws for ws in writer.book.worksheets} # get all the sheets 
+
+            existing_columns = set() # extract existing column names form the first sheet 
+            for sheet in book.worksheets:
+                if sheet.title == 'Sheet1':
+                    existing_df_sheet = pd.read_excel(self.file_name, sheet_name=sheet.title)
+                    existing_columns.update(existing_df_sheet.columns)
+
+
+            new_columns = set(self.df.columns) # check if new data has the same column names as the existing data 
+            if existing_columns == new_columns:
+                new_df = pd.concat([existing_df_sheet, self.df], axis=0, ignore_index=True)
+                last_row = len(existing_df_sheet)
+                new_df.style.apply(lambda x: ['background-color: #C4D9F2' if x.name >= last_row else ''], axis=1) # change color for only newly appended data and default white if data is old 
+                new_df.to_excel(writer, sheet_name='Sheet1', index=False)
+                sheet = book['Sheet1']
+                for row_idx, row in enumerate(sheet.iter_rows(min_row=2, min_col=1, values_only=True), start=2): # iterates over the rows of the worksheet starting from a set starting row. returns iterable of Cell objects for each row. Ensures we get the cell values instead of ALL of the cells' objects. Enumerate to get both the row index and the row values during iteration. Start =2 sets the starting value of the row index
+                    for col_idx, value in enumerate(row, 1):
+                        if row_idx > last_row + 1: 
+                            cell = sheet.cell(row = row_idx, column = col_idx)
+                            cell.fill = PatternFill(start_color='C4D9F2', end_color='C4D9F2', fill_type='solid')
+                        
+                self.adjust_col_width(sheet)
+            else:
+                self.df.to_excel(writer, sheet_name='Sheet1', index=False)
+                sheet = book['Sheet1']
+                self.adjust_col_width(sheet)
+
+    def append_multiple_dataframes_alternative(self): 
+        with pd.ExcelWriter(self.file_name, engine='openpyxl', mode='a', if_sheet_exists='replace', engine_kwargs = {'options' : {'sheet_state' : 'visible'}}) as writer:
+            try: 
+                if mimetypes.guess_type(self.file_name)[0] in ['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']:
+                    print('File is the formats: vnd.ms-excel or vnd.openxml formats')
+                    existing_df = pd.read_excel(self.file_name)
+                else: 
+                    print('File is not the in the accepted pd.read_excel formats... using alt method') 
+                    existing_file = pd.ExcelFile(self.file_name)
+                    existing_df = existing_file.parse(sheet_name = 'Sheet1')
+            except Exception as e: 
+                print(f'Error occured: {e}')
+
+            new_data = []
+            existing_columns = set() # Extract column names from the existing data into a set (different from a list because unique values only) 
+            for sheet_name, df in existing_df.items():
+                existing_columns.update(df.columns)
+
+            for df in self.df:
+                new_columns = set(df.columns) # Extract column names from the new dataframes into a set
+
+                if existing_columns.intersection(new_columns): # If there are any overlapping columns, concatenate the new data with the existing data and add to the list of new data to be written
+                    existing_df_sheet = existing_df.get(df.name, pd.DataFrame()) # if there is a dataframe name then it takes that. if there isn't then default value 
+                    last_row = len(existing_df_sheet)
+                    new_df = pd.concat([existing_df_sheet, df], axis=0, ignore_index=True)
+                    new_df.style.apply(lambda x: ['background-color: #C4D9F2' if x.name >= last_row else ''], axis=1) # temporary function for creating background colors 
+                    new_data.append(new_df)
+                    for row_idx, row in enumerate(sheet.iter_rows(min_row=2, min_col=1, values_only=True), start=2): # iterates over the rows of the worksheet starting from a set starting row. returns iterable of Cell objects for each row. Ensures we get the cell values instead of ALL of the cells' objects. Enumerate to get both the row index and the row values during iteration. Start =2 sets the starting value of the row index
+                        for col_idx, value in enumerate(row, 1):
+                            if row_idx > last_row + 1: 
+                                cell = sheet.cell(row = row_idx, column = col_idx)
+                                cell.fill = PatternFill(start_color='C4D9F2', end_color='C4D9F2', fill_type='solid')
+                else:
+                    new_data.append(df) # If there are no overlapping columns, it treats it as a new dataframe and add it to the list of dataframes 
+                
+            for df in new_data:
+                df.to_excel(writer, sheet_name=df.name, index=False)
+                sheet = writer.sheets[df.name]
+                self.adjust_col_width(sheet)
 
     def open_file(self): 
         try: 
